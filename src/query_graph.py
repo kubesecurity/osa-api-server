c289e7080c000798eef7caa17169efb4627b84ff
Arunprasad Rajkumar <ar.arunprasad@gmail.com>
2020-01-18 20:06:52 +0530

    Implement repo name based filtering

--- src/query_graph.py
+++ src/query_graph.py
@@ -2,7 +2,7 @@ from typing import get_type_hints, Dict, List
 
 from gremlin_python.structure.io import graphsonV3d0
 
-from src.graph_model import SecurityEvent, Traversel
+from src.graph_model import SecurityEvent, Dependency, Traversel
 from src.parse_datetime import from_date_str
 from src.gremlin import GREMLIN
 
@@ -29,9 +29,11 @@ def _query_template():
 def _get_security_event_query_filters(args: Dict) -> str:
     assert 'updated_at' in get_type_hints(SecurityEvent)
     query = []
-    from_date = from_date_str(args['from_date'])
-    to_date = from_date_str(args['to_date'])
-    if from_date > 0 and to_date > 0:
+    from_date = args['from_date']
+    to_date = args['to_date']
+    if from_date and to_date:
+        from_date = from_date_str(from_date)
+        to_date = from_date_str(to_date)
         query.append('''has('updated_at', between({from_date}, {to_date}))'''.format(from_date=from_date, to_date=to_date))
     return 'identity()' if len(query) is 0 else '.'.join(query)
 
@@ -40,7 +42,12 @@ def _get_probable_vul_query_filters(args: Dict) -> str:
     return 'identity()' if len(query) is 0 else '.'.join(query)
 
 def _get_dependency_query_filters(args: Dict) -> str:
+    assert 'dependency_name' in get_type_hints(Dependency)
     query = []
+    repo = args['repo']
+    if repo is not None:
+        query.append('''has('dependency_name', '{repo}')'''.format(repo=repo))
+
     return 'identity()' if len(query) is 0 else '.'.join(query)
 
 def _to_response_model(gremlin_response: Dict) -> Dict:
