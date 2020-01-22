d2e0882d87296da27f00d3835f274301f4579180
Arunprasad Rajkumar <ar.arunprasad@gmail.com>
2020-01-23 00:32:01 +0530

    Fix gunicorn run

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.app import server
 
 # import src/rest_api to register all endpoints with flask
-import src.rest_api
+from src.rest_api import app
 
 if __name__ == '__main__':
     server.run()
