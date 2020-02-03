e54712e24b471e3225cb4c792f296c9d4ef71094
Arunprasad Rajkumar <ar.arunprasad@gmail.com>
2020-02-03 17:36:26 +0530

    Add separate requirements.txt for tools

--- tools/ingest_historical_csv.py
+++ tools/ingest_historical_csv.py
@@ -20,17 +20,20 @@ async def _add_feedback(json_obj, session: ClientSession, url):# pylint: disable
     # (todo) add feedback
     pass
 
+def _get_executor(args):
+    if args.ingestion_endpoint:
+        return _ingest_probable_cve, args.ingestion_endpoint
+    return _add_feedback, args.feedback_endpoint
+
 async def _main(args):
     log.info('invoking ingestion for {} CSV files'.format(len(args.csv)))
-    func = _ingest_probable_cve if args.api_endpoint else _add_feedback
+    func, url = _get_executor(args)
     for csv in args.csv:
         log.info('Convert records in {} to JSON'.format(csv))
         obj = pd.read_csv(csv, index_col=None, header=0).to_dict(orient='records')
         log.info('Ingest {} records to DB'.format(len(obj)))
         async with ClientSession() as session:
-            await func(json_obj=obj, session=session, url=args.api_endpoint)
-    # df = pd.concat(all_df, ignore_index=True, axis=0)
-    # log.info(json.dumps(df.to_dict(orient='records')))
+            await func(json_obj=obj, session=session, url=url)
 
 def _parse_args():
     parser = argparse.ArgumentParser(prog='python',
@@ -42,7 +45,8 @@ def _parse_args():
                         nargs="+",
                         required=True)
     group = parser.add_mutually_exclusive_group()
-    group.add_argument("--api-endpoint", type=str, default="http://localhost:5000/api/v1/pcve",
+    group.add_argument("--ingestion-endpoint", type=str,
+                       default="http://localhost:5000/api/v1/pcve",
                        help="Ingestion API endpoint")
     group.add_argument("--feedback-endpoint", type=str,
                        default="http://localhost:5000/api/v1/feedback",
