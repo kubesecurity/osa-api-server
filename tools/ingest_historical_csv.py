11569cdbcd9b20f364a8e05e793dc1c789d937a1
Arunprasad Rajkumar <ar.arunprasad@gmail.com>
2020-02-05 17:14:27 +0530

    Add feedback insert support

--- tools/ingest_historical_csv.py
+++ tools/ingest_historical_csv.py
@@ -1,4 +1,4 @@
-"""Ingests historical CSV data into DB"""
+'''Ingests historical CSV data into DB'''
 
 import argparse
 import asyncio
@@ -12,45 +12,64 @@ import daiquiri
 daiquiri.setup(level=logging.INFO)
 log = daiquiri.getLogger(__name__) # pylint: disable=invalid-name
 
-async def _ingest_probable_cve(json_obj, session: ClientSession, url):
-    async with session.post(url, json=json_obj) as response:
+async def _ingest_probable_cve(df, session: ClientSession, url): # pylint: disable=invalid-name
+    obj = df.to_dict(orient='records')
+    async with session.post(url, json=obj) as response:
         log.info('Got response {} for {}'.format(response.status, url))
 
-async def _add_feedback(json_obj, session: ClientSession, url):# pylint: disable=unused-argument
-    # (todo) add feedback
-    pass
+async def _add_feedback(df, session: ClientSession, url): # pylint: disable=invalid-name
+    if len(df) < 1:
+        return
+    df['author'] = 'anonymous'
+    df['feedback_type'] = 'POSITIVE'
+    # typo carried away
+    for feedback in ('Feeedback', 'Feedback'):
+        if feedback in df:
+            df[feedback].fillna('', inplace=True)
+            df.rename(columns=dict([(feedback, 'comments')]), inplace=True)
+            break
+    else:
+        df['comments'] = ''
+    df = df[['author', 'feedback_type', 'comments', 'url']]
+    # df['comments'] = ''
+    obj = df.to_dict(orient='records')
+    async with session.post(url, json=obj) as response:
+        log.info('Got response {} for {}'.format(response.status, url))
 
 def _get_executor(args):
-    if args.ingestion_endpoint:
-        return _ingest_probable_cve, args.ingestion_endpoint
-    return _add_feedback, args.feedback_endpoint
+    if args.feedback:
+        return _add_feedback, args.feedback
+    return _ingest_probable_cve, args.insert
 
 async def _main(args):
     log.info('invoking ingestion for {} CSV files'.format(len(args.csv)))
     func, url = _get_executor(args)
     for csv in args.csv:
         log.info('Convert records in {} to JSON'.format(csv))
-        obj = pd.read_csv(csv, index_col=None, header=0).to_dict(orient='records')
-        log.info('Ingest {} records to DB'.format(len(obj)))
+        df = pd.read_csv(csv, index_col=None, header=0) # pylint: disable=invalid-name
+        log.info('Ingest {} records to DB'.format(len(df)))
         async with ClientSession() as session:
-            await func(json_obj=obj, session=session, url=url)
+            await func(df=df, session=session, url=url)
 
 def _parse_args():
     parser = argparse.ArgumentParser(prog='python',
                                      description=textwrap.dedent('''\
                                     This script can be used to ingest historical CSV data
                                     into database using API'''))
-    parser.add_argument("--csv",
-                        help="Glob path of CSV files which has to be ingested into DB",
-                        nargs="+",
-                        required=True)
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument("--ingestion-endpoint", type=str,
-                       default="http://localhost:5000/api/v1/pcve",
-                       help="Ingestion API endpoint")
-    group.add_argument("--feedback-endpoint", type=str,
-                       default="http://localhost:5000/api/v1/feedback",
-                       help="Feedback API endpoint")
+    parser.add_argument('csv',
+                        help='Glob path of CSV files which has to be ingested into DB',
+                        nargs='+')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--insert',
+                       type=str,
+                       nargs='?',
+                       const='http://localhost:5000/api/v1/pcve',
+                       help='API endpoint to use for the operation')
+    group.add_argument('--feedback',
+                       type=str,
+                       nargs='?',
+                       const='http://localhost:5000/api/v1/feedback',
+                       help='API endpoint to use for adding feedback')
     return parser.parse_args()
 
 if __name__ == '__main__':
