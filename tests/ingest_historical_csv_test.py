227818b32866635cc0a2379240cf31c30e4eeadf
Arunprasad Rajkumar <ar.arunprasad@gmail.com>
2020-02-09 13:22:45 +0530

    Add test place holder

---
+++ tests/ingest_historical_csv_test.py
@@ -0,0 +1,12 @@
+"""To test historical data insertion script"""
+import pytest
+
+@pytest.mark.asyncio
+async def test_insertion():
+    """To test insertion"""
+    # (todo)
+
+@pytest.mark.asyncio
+async def test_feedback():
+    """To test feedback"""
+    # (todo)
