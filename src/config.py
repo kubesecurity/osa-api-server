c367435d809007e0ee07a99f272f893c4a5d8ebb
Arunprasad Rajkumar <ar.arunprasad@gmail.com>
2020-02-09 13:26:24 +0530

    Access gremlin server directly

--- src/config.py
+++ src/config.py
@@ -3,3 +3,4 @@ import os
 
 GREMLIN_DEFAULT_HOST = os.environ.get('GREMLIN_DEFAULT_HOST', 'localhost')
 GREMLIN_DEFAULT_PORT = os.environ.get('GREMLIN_DEFAULT_PORT', 8182)
+GREMLIN_SCHEME = os.environ.get('GREMLIN_SCHEME', 'http')
--- src/gremlin.py
+++ src/gremlin.py
@@ -1,14 +1,30 @@
 """Abstracts access to gremlin server through REST calls"""
 
+import json
+import logging
+import requests
+import daiquiri
 from gremlin_python.structure.io import graphsonV3d0
+from src.config import GREMLIN_DEFAULT_HOST, GREMLIN_DEFAULT_PORT, GREMLIN_SCHEME
 
-from gremlin_connect.gremlin_adapter import GremlinAdapter
-from src.config import GREMLIN_DEFAULT_HOST, GREMLIN_DEFAULT_PORT
+daiquiri.setup(level=logging.DEBUG)
+log = daiquiri.getLogger(__name__) # pylint: disable=invalid-name
 
-_GREMLIN = GremlinAdapter(GREMLIN_DEFAULT_HOST, int(GREMLIN_DEFAULT_PORT))
 _READER = graphsonV3d0.GraphSONReader()
+_GREMLIN_URL = '{scheme}://{host}:{port}'.format(scheme=GREMLIN_SCHEME,
+                                                 host=GREMLIN_DEFAULT_HOST,
+                                                 port=GREMLIN_DEFAULT_PORT)
+def test_connection() -> bool:
+    """Test connection to gremlin server."""
+    response = requests.get(_GREMLIN_URL)
+    # Everything except "not found" is acceptable.
+    return response.status_code != 404
 
 def execute_query(query):
     """Posts given Traversel to gremlin server"""
-    response = _GREMLIN.execute_query(str(query))
-    return _READER.toObject(response)
+    response = requests.post(_GREMLIN_URL,
+                             data=json.dumps({"gremlin": str(query)}))
+    log.info('Gremin Query:"{}", status_code:{}'.format(query,
+                                                        response.status_code))
+    response.raise_for_status()
+    return _READER.toObject(response.json())
