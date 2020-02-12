8fc4ac6bd75788e62ad89a6d21a7dd208f3f527d
Arunprasad Rajkumar <ar.arunprasad@gmail.com>
2020-02-12 12:25:51 +0530

    Add testcase for None and List with None

--- tests/src/sanitizer_test.py
+++ tests/src/sanitizer_test.py
@@ -1,9 +1,17 @@
 """To test sanitizer"""
+
+import pytest
+
 from src.sanitizer import sanitize, unsanitize
 
 # pylint: disable=missing-function-docstring
 def test_string_sanitization_with_empty():
     assert sanitize('') == ''
+
+def test_string_sanitization_with_none():
+    with pytest.raises(Exception) as exception:
+        sanitize(None)
+    assert "Unexpected type" in str(exception.value)
 
 def test_string_sanitization_with_normal_chars():
     assert sanitize('foo') == 'foo'
@@ -14,6 +22,10 @@ def test_string_sanitization_with_special_chars():
 def test_string_sanitization_with_empty_list():
     assert sanitize([]) == ''
 
+def test_string_sanitization_with_none_list():
+    with pytest.raises(Exception):
+        sanitize([None])
+
 def test_string_sanitization_with_a_list():
     assert sanitize(['a', 'b']) == "'a', 'b'"
 
@@ -22,6 +34,10 @@ def test_string_sanitization_with_special_char_list():
 
 def test_string_sanitization_with_special_char_list_1():
     assert sanitize(['foo)"bar', 'b b']) == "'foo%29%22bar', 'b%20b'"
+
+def test_string_unsanitization_with_none():
+    with pytest.raises(Exception):
+        assert unsanitize(None) == ''
 
 def test_string_unsanitization_with_empty():
     assert unsanitize('') == ''
