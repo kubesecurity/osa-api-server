4e2a9f2a426d5afce3b5585b1cd966ade1231b08
Arunprasad Rajkumar <ar.arunprasad@gmail.com>
2020-02-14 12:57:34 +0530

    Add script to ingest historical data (#4)
    * Add more info to README.md

--- README.md
+++ README.md
@@ -1,3 +1,60 @@
-# OSA API Server
+# API Server
 
 ![build and test](https://github.com/fabric8-analytics/osa-api-server/workflows/build%20and%20test/badge.svg)
+
+## Development
+
+Make sure you have `Python >= 3.6` installed in your development environment,
+
+Install `virtualenv`, activate it and then install project dependencies
+
+```
+pip install -r requirements.txt
+```
+
+Run API server,
+```
+gunicorn --pythonpath . -k sync -b localhost:5000 main:app --reload
+```
+
+Swagger/OpenAPI based documentation can be obtained by accessing the following URL,
+
+[http://localhost:5000/api/swagger](http://localhost:5000/api/swagger)
+
+Run unittest,
+```
+pip install -r requirements-test.txt
+pytest
+```
+
+### To Deploy it on OpenShift,
+
+- Build container image (e.g. docker)
+- Push container image to registry (e.g. quay.io)
+- use `oc` to deploy it on OpenShift,
+```
+docker build --no-cache -t <image_name:tag> .
+docker push <registry>/<image_name:tag>
+oc process -f openshift/template.yaml -p DOCKER_REGISTRY=<registry> -p DOCKER_IMAGE=<image_name> -p GREMLIN_DEFAULT_HOST=<gremlin_host> -p GREMLIN_DEFAULT_PORT=<gremlin_port> | oc create -f -
+```
+
+## Ingest historical data
+
+A [command tool](tools/ingest_historical_csv.py) has been created to ingest historical data into database through API server,
+
+Make sure dependencies of [command tool](tools/ingest_historical_csv.py) has been installed in your development `virtualenv`,
+
+```
+pip install -r tools/requirements.txt
+```
+
+#### To ingest CSV to DB,
+
+```
+python tools/ingest_historical_csv.py <path_to_csv_file> --insert
+```
+
+#### To ingest Feedback,
+```
+python tools/ingest_historical_csv.py <path_to_csv_file> --feedback
+```
---
+++ tools/ingest_historical_csv.py
@@ -0,0 +1,107 @@
+'''Ingests historical CSV data into DB'''
+
+from typing import Dict
+import argparse
+import asyncio
+import textwrap
+
+from aiohttp import ClientSession
+import pandas as pd
+import daiquiri
+
+log = daiquiri.getLogger(__name__) # pylint: disable=invalid-name
+
+_failing_list: Dict[str, str] = {}
+
+def _report_failures():
+    if len(_failing_list) == 0:
+        log.info("Successfully ingested")
+    for k, v in _failing_list.items(): # pylint: disable=invalid-name
+        log.error("'{}' failed with status '{}'".format(k, v))
+
+async def _insert_df(df, session: ClientSession, url, csv, sem): # pylint: disable=invalid-name
+    objs = df.to_dict(orient='records')
+    for obj in objs:
+        async with sem, session.post(url, json=obj) as response:
+            log.debug('Got response {} for {}'.format(response.status, obj))
+            if response.status != 200:
+                log.error('Error response {} for {}'.format(response.status, obj))
+                _failing_list.update(dict([(csv, response.status)]))
+
+async def _add_feedback(df, session: ClientSession, url, csv, sem): # pylint: disable=invalid-name
+    if len(df) < 1:
+        return
+    df['author'] = 'anonymous'
+    # typo carried away
+    for feedback in ('Feeedback', 'Feedback'):
+        if feedback in df:
+            df[feedback].fillna('', inplace=True)
+            df.rename(columns=dict([(feedback, 'comments')]), inplace=True)
+            break
+    else:
+        df['comments'] = ''
+    # old feedback has false positives with comments, transform it to
+    # POSITIVE or NEGATIVE
+    tx_comment = lambda x: ('NEGATIVE' if x.lower().startswith('no') else 'POSITIVE')
+    df['feedback_type'] = df['comments'].apply(tx_comment)
+    df = df[['author', 'feedback_type', 'comments', 'url']]
+    await _insert_df(df, session, url, csv, sem)
+
+def _get_executor(args):
+    if args.feedback:
+        return _add_feedback, args.feedback
+    return _insert_df, args.insert
+
+async def _main(args):
+    daiquiri.setup(level=("DEBUG" if args.verbose else "INFO"))
+    log.info('invoking ingestion for {} CSV files'.format(len(args.csv)))
+    func, url = _get_executor(args)
+    sem = asyncio.BoundedSemaphore(args.concurrency)
+    async with ClientSession() as session:
+        tasks = []
+        for csv in args.csv:
+            log.debug('Convert records in {} to JSON'.format(csv))
+            df = pd.read_csv(csv, index_col=None, header=0) # pylint: disable=invalid-name
+            log.debug('Ingest {} records to DB'.format(len(df)))
+            task = asyncio.ensure_future(func(df=df, session=session, url=url,
+                                              csv=csv, sem=sem))
+            tasks.append(task)
+        _ = await asyncio.gather(*tasks)
+    _report_failures()
+
+def _parse_args():
+    parser = argparse.ArgumentParser(prog='python',
+                                     description=textwrap.dedent('''\
+                                    This script can be used to ingest historical CSV data
+                                    into database using API'''))
+    parser.add_argument('csv',
+                        help='Glob path of CSV files which has to be ingested into DB',
+                        nargs='+')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--insert',
+                       type=str,
+                       nargs='?',
+                       const='http://localhost:5000/api/v1/pcve',
+                       help='API endpoint to use for the operation')
+    group.add_argument('--feedback',
+                       type=str,
+                       nargs='?',
+                       const='http://localhost:5000/api/v1/feedback',
+                       help='API endpoint to use for adding feedback')
+    parser.add_argument('--concurrency',
+                        type=int,
+                        default=10,
+                        help='No of concurrent requests allowed')
+    parser.add_argument('--verbose', '-v',
+                        action='store_true',
+                        help='increase output verbosity')
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    # pylint: disable=invalid-name
+    loop = asyncio.get_event_loop()
+    # (todo) Use asyncio.run after moving to Python 3.7+
+    try:
+        loop.run_until_complete(_main(args=_parse_args()))
+    finally:
+        loop.close()
