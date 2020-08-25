cf2c7b780af0e32a9c8159aab643ad6bdf2be6a5
Raj Zalavadia <rzalavad@redhat.com>
2020-08-25 11:34:30 +0530

    Integrated api code with the Sentry. (#20)

--- src/app.py
+++ src/app.py
@@ -1,8 +1,17 @@
 """Module that encapsulates flask app and flask_restplus api object creation."""
+import sentry_sdk
 from flask import Flask
 from flask_restplus import Api
-
+from sentry_sdk.integrations.flask import FlaskIntegration
 from werkzeug.middleware.proxy_fix import ProxyFix
+
+from src import config
+
+# initialize sentry for error logging
+sentry_sdk.init(
+    dsn=config.SENTRY_DSN,
+    integrations=[FlaskIntegration()]
+)
 
 app = Flask(__name__)  # pylint: disable=invalid-name
 
